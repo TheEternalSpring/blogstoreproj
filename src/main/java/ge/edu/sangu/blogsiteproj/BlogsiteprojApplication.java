76bd2e549d01cdae65395ccc3806244bdf8794d2
TheEternalSpring <sababuch@gmail.com>
2026-06-26 18:57:13 +0400

    first commit

---
+++ src/main/java/ge/edu/sangu/blogsiteproj/BlogsiteprojApplication.java
@@ -0,0 +1,13 @@
+package ge.edu.sangu.blogsiteproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsiteprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogsiteprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ge/edu/sangu/blogsiteproj/BlogsiteprojApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.edu.sangu.blogsiteproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsiteprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
